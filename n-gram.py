2775ca37777dcbdce8e8fd6f25dad32d82e9df61
Fitzberth Powell Channer <fitzberth.powellchanner@ucr.ac.cr>
2022-06-11 18:58:06 -0600

    n-grama terminado

--- n-gram.py
+++ n-gram.py
@@ -1,7 +1,9 @@
+from cmath import e
 from operator import indexOf
+from tokenize import Double
 from typing import OrderedDict
 
-from numpy import matrix
+from numpy import double, matrix
 
 import random
 
@@ -24,6 +26,7 @@ def add_decorator(words,decorator,n):
     for word in words:
         decorated_word = decorator_string + word.lower() + decorator_string #agrega los decoradores al inicio y final del string
         decorated_list.append(decorated_word) #agrega a la lista
+        
     return decorated_list #retorna la lista
 
 def get_sequences(words, n):
@@ -65,11 +68,14 @@ def create_model(words, ngrams):
 def generate_word(model,seed):
     r = random.Random()
     r.seed(seed)
+    #prepares the data to be used
     sequence = model[1]
     transition_matrix = model[0]
     current_sequence = sequence[0]
     generated_word = "" + current_sequence
     sequence_size = len(current_sequence)
+    
+    #creates the word
     while(current_sequence != sequence[0] or len(generated_word) == sequence_size):
         value = r.random()
         counter = 0
@@ -83,14 +89,45 @@ def generate_word(model,seed):
                 break
 
     generated_word = generated_word[sequence_size:-sequence_size].capitalize() #curates the word
-    return generated_word
+
+    return generated_word #returns the word
 
 def get_probability(model, word):
-    e = None
+    sequence_size = len(model[1][0])
+    decorator = ""
+    for i in range(sequence_size):
+        decorator += "$"
+    decorated_word = decorator + word + decorator
+    word_sequence = []
 
+    for i in range(len(decorated_word)-sequence_size):
+        word_sequence.append(decorated_word[i:i+sequence_size])
+    word_sequence = list(OrderedDict.fromkeys(word_sequence))
+    word_sequence.sort()
+    for sequence in word_sequence:
+       if len(sequence) < sequence_size:
+            word_sequence.remove(sequence)
+    
+
+    model_sequence = model[1]
+    transition_matrix = model[0]
+    probaility = 1.0
+    for i in range (len(decorated_word)-sequence_size):
+        probaility *= transition_matrix[model_sequence.index(decorated_word[i:i+sequence_size])][model_sequence.index(decorated_word[i+1:i+1+sequence_size])]
+
+    return probaility
 
 words = load_words("pokemon.csv")
 
-modelo = create_model(words, 3) 
+modelo = create_model(words, 2) 
 
-print(generate_word(modelo,21))
+print(generate_word(modelo,15))
+
+
+
+#a. a medida que aumenta el N se obtienen mas combinaciones posibles en la matriz de transicion que de otra manera no existirian
+#
+#b. nombre interesante? Cartle parece mezcla de carro y tortle , 2 Ngramas semilla 597, que no tenga sentido? Wancidoruxiena, 2 ngrama, numero 15 el nombre no termina de parecer algo
+#
+#c. dado que solo importa en cual secuencia me encuentro para elegir la siguiente es la propiedad markoviana que el estado actual solo depende del estado anterior
+#
